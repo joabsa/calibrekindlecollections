dd3de6aaa674767ed7c62bfd1fad5e359a861784
Abraham Joseph <joabsa69@gmail.com>
2011-03-01 21:50:15 -0600

    added logging

--- CalibreKindleCollections.py
+++ CalibreKindleCollections.py
@@ -24,7 +24,7 @@ Please feel free to use and extend the script in any way you want. If you alread
 
 import sys,os,json,time
 from hashlib import sha1
-from optparse import OptionParser
+import logging
 
 
 # Relative path to collection json file
@@ -41,6 +41,9 @@ calibreC = {}
 
 # Dictionary for command-line options
 options = {}
+
+# logger for the application
+log = logging.getLogger("app")
 
 def setup():
     ''' 
@@ -59,9 +62,26 @@ def setup():
     (options,args) = parser.parse_args()    
     options.excludeTags = options.excludeTags.split(',')
     
+
     # Use the mount point 
     COLLECTIONS = os.path.join(options.mntPoint,COLLECTIONS)
     CALIBRE = os.path.join(options.mntPoint,CALIBRE)
+
+    # setup the logger
+    if options.quiet:
+        log.setLevel(logging.ERROR)
+    elif options.verbose:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter("%(message)s")
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
+    log.debug("Log level: %s"%log.getEffectiveLevel())
+
     
 def loadCalibre():
     ''' Loads/Transform Calibre metadata in dictionary '''
@@ -112,7 +132,7 @@ def loadCalibre():
         calibreC = colls.values()
 
     except Exception, err:
-        sys.stderr.write('ERROR loadCalibre: %s\n'%str(err))
+        log.error("loadCalibre: %s"%str(err))
         exit()
     
 def loadCollections():
@@ -197,7 +217,7 @@ def updateCollections():
                 
             # print a description of the collection 
             collDesc = '%s:\n%s\n'%(cName[0:cName.find('@en')],'\t'+'\n\t'.join(titles))
-            print collDesc.encode('utf-8')
+            log.info(collDesc.encode('utf-8'))
 
 if __name__ == '__main__':
 
@@ -205,7 +225,7 @@ if __name__ == '__main__':
     
     # Check if path are correct
     if not (os.path.exists(COLLECTIONS) and os.path.exists(CALIBRE)):
-        print 'ERROR: unknown path to Kindle mounting point. Please set `%s` to correct path (e.g. /Volumes/Kindle)'%(options.mntPoint)
+        log.error("Unknown path to Kindle mounting point. Please correct path (e.g. /Volumes/Kindle) on the command line")
         sys.exit() 
         
     # Load collections
